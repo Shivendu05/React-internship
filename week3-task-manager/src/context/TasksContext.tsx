0a3f938ca45646c1c4bf60c801db8b6ecf334319
Shivendu05 <shivendu.mishra2005@gmail.com>
2026-02-01 17:55:06 +0530

    Delete week3-task-manager directory

--- week3-task-manager/src/context/TasksContext.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from 'react';
-import { useTasksStore } from '../hooks/useTasksStore';
-import { TasksContext } from './tasksStoreContext';
-
-export function TasksProvider({ children }: { children: ReactNode }) {
-  const store = useTasksStore();
-  return <TasksContext.Provider value={store}>{children}</TasksContext.Provider>;
-}
